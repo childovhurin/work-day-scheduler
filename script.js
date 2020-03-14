7efb8a12dd5fc3f5d304533f8936aff89c3a035a
Megan Murphy <childovhurin@gmail.com>
2020-03-14 19:28:34 -0400

    first fucntion console logged

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-
-//VARIABLES
-var currentTime = m.format('MMMM Do YYYY, h:mm:ss a');
-var currentDate = m.format("MMM Do YYYY");
-var currentHour = moment().hours();
+$(document).ready(function () {
+    //VARIABLES
+    var m = moment();
+    var currentTime = m.format('MMMM Do YYYY, h:mm:ss a');
+    var currentDate = m.format("MMM Do YYYY");
+    var currentHour = moment().hours();
     console.log('current hour: ', currentHour)
 
+});
